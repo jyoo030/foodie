7388743c8768b5eb1fdcf9b5d8fda6c67aac5eff
yimjaehyun <jaehyunyim1997@gmail.com>
2020-03-19 12:47:47 -0700

    Create seperate views for buttons

---
+++ foodie/Buttons/LikeButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  LikeButtonView.swift
+//  foodie
+//
+//  Created by Justin Yoo on 3/16/20.
+//  Copyright © 2020 Joftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct LikeButtonView: View {
+    var body: some View {
+          Button(
+              action: {
+                  // do something
+              },
+              label: { Image("like_button").renderingMode(.original)
+                  .resizable()
+                  .frame(width: 40.0, height: 40.0)
+                  .aspectRatio(contentMode: .fit)
+                  .padding(10)
+              }
+          )
+          .background(Color.white)
+          .clipShape(Circle())
+          .frame(width: 60.0, height: 60.0)
+          .shadow(radius: 7)
+    }
+}
+
+struct LikeButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        LikeButtonView()
+    }
+}
--- foodie/ContentView.swift
+++ foodie/ContentView.swift
@@ -14,45 +14,15 @@ struct ContentView: View {
             Spacer()
             HStack {
                 Spacer()
-                
-                // Dislike Button
-                Button(
-                    action: {
-                        // do something
-                    },
-                    label: { Image("dislike_button").renderingMode(.original)
-                        .resizable()
-                        .frame(width: 40.0, height: 40.0)
-                        .aspectRatio(contentMode: .fit)
-                        .padding(10)
-                    }
-                )
-                .background(Color.white)
-                    .clipShape(Circle())
-                .frame(width: 60.0, height: 60.0)
-                .shadow(radius: 7)
+            
+                DislikeButtonView()
                 
                 Spacer()
             
-                // Like Button
-                Button(
-                    action: {
-                        // do something
-                    },
-                    label: { Image("like_button").renderingMode(.original)
-                        .resizable()
-                        .frame(width: 40.0, height: 40.0)
-                        .aspectRatio(contentMode: .fit)
-                        .padding(10)
-                    }
-                )
-                .background(Color.white)
-                .clipShape(Circle())
-                .frame(width: 60.0, height: 60.0)
-                .shadow(radius: 7)
+                LikeButtonView()
+                
                 Spacer()
             }
-            
         }
         .padding(.bottom, 100)
         .background(Color(red: 240/255, green: 240/255, blue: 240/255))
