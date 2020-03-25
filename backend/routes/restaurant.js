3d91eae24d2a4c9acabe2d69076ae9f6b9ae70e6
yimjaehyun <jaehyunyim1997@gmail.com>
2020-03-24 21:26:16 -0700

    Add yelp endpoints

--- backend/routes/restaurant.js
+++ backend/routes/restaurant.js
@@ -1,12 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const yelp = require('yelp-fusion');
+const router = express.Router();
+
+require('dotenv').config()
+const client = yelp.client(process.env.YELP_API_KEY);
 
 router.get('/id/:id', function (req, res) {
-  res.send(req.params.id)
+	client.business(req.params.id).then(response => {
+		res.json(response.body);
+	}).catch(e => {
+		console.log(e);
+	});
 })
 
-router.get('/radius/:radius', function(req, res){
-  res.send(req.params.radius)
+router.get('/radius/:radius', function(req, res) {
+	if(!req.query.location)
+		res.send("Missing location parameter.")
+
+	const searchRequest = {
+		term: 'restaurants',
+		location: req.query.location,
+		radius: req.params.radius
+	};
+
+	client.search(searchRequest).then(response => {
+		res.json(response.body).status(200);
+	}).catch(e => {
+		console.log(e);
+	});
 });
 
 //export this router to use in our index.js
