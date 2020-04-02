5d4341f587cf390ddef5e7e4601f5ed4151ad89a
jyoo030 <jyoo030@ucr.edu>
2020-04-01 18:44:20 -0700

    Add "NAH" and "YUMMY" to cards when swiping

--- foodie/Cards/CardView.swift
+++ foodie/Cards/CardView.swift
@@ -12,8 +12,13 @@ import KingfisherSwiftUI
 struct CardView: View {
     private var onRemove: (_ Restaurant: Restaurant) -> Void
     private var restaurant: Restaurant
-    var thresholdPercentage: CGFloat = 0.5
+    var thresholdPercentage: CGFloat = 0.3
     @State private var translation: CGSize = .zero
+    @State private var status: yumOrNah = .none
+    
+    private enum yumOrNah: Int {
+        case yum, nah, none
+    }
     
     init(restaurant: Restaurant, onRemove: @escaping (_ Restaurant: Restaurant) -> Void) {
         self.restaurant = restaurant
@@ -27,10 +32,36 @@ struct CardView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack(spacing:0) {
-                KFImage(URL(string: self.restaurant.image_url)!)
-                .resizable()
-                    .aspectRatio(contentMode: .fill)
-                .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.55)
+                ZStack(alignment: self.status == .yum ? .topLeading : .topTrailing) {
+                    KFImage(URL(string: self.restaurant.image_url)!)
+                    .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.55)
+                    
+                    if self.status == .yum {
+                        Text("YUMMY")
+                            .font(.headline)
+                        .padding()
+                        .cornerRadius(10)
+                            .foregroundColor(Color.orange)
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.orange, lineWidth: 3.0))
+                            .padding(24)
+                            .rotationEffect(Angle.degrees(-45))
+                    }  else if self.status == .nah {
+                       Text("NAH FAM")
+                           .font(.headline)
+                           .padding()
+                           .cornerRadius(10)
+                           .foregroundColor(Color.blue)
+                           .overlay(
+                               RoundedRectangle(cornerRadius: 10)
+                                   .stroke(Color.blue, lineWidth: 3.0)
+                       ).padding(.top, 45)
+                           .rotationEffect(Angle.degrees(45))
+                    }
+                }
 
                 VStack(alignment: .leading) {
                     HStack {
@@ -71,6 +102,14 @@ struct CardView: View {
                  DragGesture()
                  .onChanged { value in
                      self.translation = value.translation
+                    if (self.getGesturePercentage(geometry, from: value)) >= self.thresholdPercentage {
+                        self.status = .yum
+                    } else if (self.getGesturePercentage(geometry, from: value)) <= -self.thresholdPercentage {
+                        self.status = .nah
+                    } else {
+                        self.status = .none
+                    }
+                    
                  }
                  .onEnded { value in
                     if abs(self.getGesturePercentage(geometry, from: value)) > self.thresholdPercentage {
