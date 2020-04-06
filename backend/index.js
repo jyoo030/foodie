649720768f3379b371db3b0122b58705e3fa71a4
yimjaehyun <jaehyunyim1997@gmail.com>
2020-04-06 15:37:16 -0700

    Connect to mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,28 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+"use strict";
 
-var restaurants = require('./routes/restaurant');
+var util = require("util");
+
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(
+        process.env.DB_CONNECTION_STRING,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log("connected to DB!")
+    )
+    .catch(err => console.log(err));
+
+var app = express();
+app.use(bodyParser.json());
+
+// Routes
+app.use('/restaurant', require('./routes/restaurant'));
+app.use("/user", require("./routes/user"));
+
 
 app.listen(port, () => console.log(`foodieAPI listening on port ${port}!`));
-
-app.use('/restaurant', restaurants);
-
-
