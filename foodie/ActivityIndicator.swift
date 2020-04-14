399d900ac4f133278d487bddc96efc30be254520
yimjaehyun <jaehyunyim1997@gmail.com>
2020-04-13 18:02:10 -0700

    Reorganize file structure

---
+++ foodie/ActivityIndicator.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityIndicator.swift
+//  foodie
+//
+//  Created by Jae Hyun on 4/2/20.
+//  Copyright © 2020 Joftware. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
