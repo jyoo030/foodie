bc6d043f78bed12210623a20a93188a52c44f8a8
yimjaehyun <jaehyunyim1997@gmail.com>
2020-04-18 18:44:51 -0700

    Fix Spacing and remove unnecessary Vstack

--- foodie/ContentView.swift
+++ foodie/ContentView.swift
@@ -18,55 +18,52 @@ struct ContentView: View {
         NavigationView {
             if !self.userDefaultsManager.userId.isEmpty {
                 GeometryReader { g in
-                ZStack {
-                    VStack {
-                           GeometryReader { geometry in
-                               ZStack {
-                                HeaderView(addGroupToggle: self.$addGroupToggle)
-                                    .offset(y: -geometry.size.height*0.44)
-                                                           
-                                BottomCard()
-                                    .offset(y: -geometry.size.height*0.044)
-                                   
-                                CardStackView(swipeVar: self.swipeVar)
-                                    .offset(y: -geometry.size.height*0.03)
-                                                                 
-                               Spacer()
-                           
-                               HStack {
-                                DislikeButtonView(swipeVar: self.swipeVar)
-                                   Spacer()
-                                LikeButtonView(swipeVar: self.swipeVar)
-                               }.offset(y:geometry.size.height*0.35)
-                           }
-                            
-                            VStack {
-                                Spacer()
-                                FooterView()
-                            }.padding(.bottom, 40)
-                            
-                       }
-                    }
-                    .zIndex(0)
-                    .background(self.addGroupToggle ? Color.gray : Color(red: 240/255, green: 240/255, blue: 240/255))
-                    .cornerRadius(self.addGroupToggle ? 30 : 0)
-                    .scaleEffect(x: self.addGroupToggle ? 0.95 : 1, y: self.addGroupToggle ? 0.95 : 1, anchor: .center)
-                    .frame(height: self.addGroupToggle ? g.size.height * 0.87 : g.size.height)
-                    .onAppear(perform: {
-                        if self.userDefaultsManager.name.isEmpty {
-                            self.userManager.getUser(id: self.userDefaultsManager.userId)
-                        }
-                    })
-                        
-                    if self.addGroupToggle {
+                    ZStack {
                         VStack {
-                            Spacer()
-                            AddGroupView(addGroupToggle: self.$addGroupToggle)
+                               GeometryReader { geometry in
+                                   ZStack {
+                                    HeaderView(addGroupToggle: self.$addGroupToggle)
+                                        .offset(y: -geometry.size.height*0.44)
+                                                               
+                                    BottomCard()
+                                        .offset(y: -geometry.size.height*0.044)
+                                       
+                                    CardStackView(swipeVar: self.swipeVar)
+                                        .offset(y: -geometry.size.height*0.03)
+                                                                     
+                                   Spacer()
+                               
+                                   HStack {
+                                    DislikeButtonView(swipeVar: self.swipeVar)
+                                       Spacer()
+                                    LikeButtonView(swipeVar: self.swipeVar)
+                                   }.offset(y:geometry.size.height*0.35)
+                               }
+                                
+                                VStack {
+                                    Spacer()
+                                    FooterView()
+                                }.padding(.bottom, 40)
+                                
+                           }
                         }
-                            .zIndex(1)
-                            .transition(.move(edge: .bottom))
+                        .zIndex(0)
+                        .background(self.addGroupToggle ? Color.gray : Color(red: 240/255, green: 240/255, blue: 240/255))
+                        .cornerRadius(self.addGroupToggle ? 30 : 0)
+                        .scaleEffect(x: self.addGroupToggle ? 0.95 : 1, y: self.addGroupToggle ? 0.95 : 1, anchor: .center)
+                        .frame(height: self.addGroupToggle ? g.size.height * 0.87 : g.size.height)
+                        .onAppear(perform: {
+                            if self.userDefaultsManager.name.isEmpty {
+                                self.userManager.getUser(id: self.userDefaultsManager.userId)
+                            }
+                        })
+                            
+                        if self.addGroupToggle {
+                            AddGroupView(addGroupToggle: self.$addGroupToggle)
+                                .zIndex(1)
+                                .transition(.move(edge: .bottom))
+                        }
                     }
-                }
                 }
                 .edgesIgnoringSafeArea(.top)
             } else {
