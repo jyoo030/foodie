9bdf6d92a1b40be57bee10fb3e3396f3aabbd216
yimjaehyun <jaehyunyim1997@gmail.com>
2020-05-04 23:30:00 -0700

    Add Socket.io

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const port = 3000;
 const mongoose = require("mongoose");
+const http = require('http');
+const socketio = require('socket.io');
+
+var currentSockets = {}
+const Notification = require("./models/notification");
 
 mongoose
     .connect(
@@ -17,13 +22,52 @@ mongoose
     )
     .catch(err => console.log(err));
 
-var app = express();
-app.use(bodyParser.json());
+const app = express();
 
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(bodyParser.json());
 // Routes
 app.use('/restaurant', require('./routes/restaurant'));
 app.use('/user', require('./routes/user'));
 app.use('/group', require('./routes/group'));
+app.use('/notification', require('./routes/notification'));
 
+io.on('connect', socket => {
+	console.log('new connection');
 
-app.listen(port, () => console.log(`foodieAPI listening on port ${port}!`));
+    socket.on("user_online", (data) => {
+        const {userId, socketId} = data 
+        currentSockets[userId] = socketId;
+        console.log(JSON.stringify(currentSockets))
+    });
+
+    socket.on("disconnect", () => {
+        let key = Object.keys(currentSockets).find(key => currentSockets[key] === socket.id);
+        delete currentSockets[key]
+        console.log(JSON.stringify(currentSockets))
+    })
+
+    socket.on("friend_request", (data) => {
+        console.log("friend_request")
+
+        // Check user is online
+        if (Object.keys(io.sockets.sockets).includes(data.to)) {
+            io.to(data.to).emit('test', 'test')
+        }
+
+        // Send Push Notification
+        // Store in NotificationSchema
+
+        var newNotification = Notification({
+            "sender": data.from,
+            "reciever": data.to,
+            "message": "friend_request"
+        })
+
+        newNotification.save();
+    })
+});
+
+server.listen(port, () => console.log(`foodieAPI listening on port ${port}!`));
